ebf21a974ad1a9a17f924c89ea844e5b94e80e45
Simon Tharby <jinjamail@gmail.com>
2019-04-20 14:37:37 +0100

    configure webpack to load moment npm package (using 'require' in index.js)

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ let para = document.getElementById('para');
 para.innerHTML = 'hello world!'
 
 // index.js
+var moment = require('moment');
+
 console.log("Hello from JavaScript!");
-//console.log(moment().startOf('day').fromNow());
+console.log(moment().startOf('day').fromNow());
